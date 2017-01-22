580452144888c720ccf7f5c1f6631a4d079abba9
Phoenix Zhang <phoenix.nir@gmail.com>
2017-01-22 16:27:03 +0800

    init

---
+++ test/rocks/pho/btc/thinker_test.clj
@@ -0,0 +1,7 @@
+(ns rocks.pho.btc.thinker-test
+  (:require [clojure.test :refer :all]
+            [rocks.pho.btc.thinker :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
