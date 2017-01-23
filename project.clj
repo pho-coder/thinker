2739c75c0ff5174ed97eadbeb1091942ed16dd04
Phoenix Zhang <phoenix.nir@gmail.com>
2017-01-23 21:42:30 +0800

    fix

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot rocks.pho.btc.thinker
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [mount "0.1.11"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.7"]]
+  :main rocks.pho.btc.thinker
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/rocks/pho/btc/thinker.clj
+++ src/rocks/pho/btc/thinker.clj
@@ -1,7 +1,8 @@
 (ns rocks.pho.btc.thinker
+  (:require [clojure.tools.logging :as log])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (log/info "Hello, World!"))
